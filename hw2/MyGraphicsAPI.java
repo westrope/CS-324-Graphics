8d4913c7f8d299f5be871292ef8eb7c077b36d2a
Ian Westrope <west2737@vandals.uidaho.edu>
2014-02-12 16:46:20 -0800

    Created window class and set functions

--- hw2/MyGraphicsAPI.java
+++ hw2/MyGraphicsAPI.java
@@ -14,12 +14,20 @@ public class MyGraphicsAPI extends Jcomponent
 {
     int framewidth = 500;
     int frameheight = 500;
-    int viewportwidth;
-    int viewportheight;
-    int windowheight;
-    int widnowwidth;
+    int viewportwidth1;
+    int viewportwidth2;
+    int viewportheight1;
+    int viewportheight2;
     float x;
     float y;
+
+    class window
+    {
+    float windowheight1;
+    float windnowwidth1;
+    float windowheight2;
+    float windowwidth2;
+    }
 
     public void InitGraphics()
     {
@@ -36,20 +44,25 @@ public class MyGraphicsAPI extends Jcomponent
 	f.setVisible( true );
 	f.getContentPane().add( new HW2() );
 
-	SetViewport( 1 , 1 );
-	SetWindow(
+	SetViewport( -1 , -1, 1, 1 );
 
     }
 
-    public void SetViewport( int width, int height  )
+    public void SetViewport( int width1, int height1, int width2, int height2  )
     {
-	viewportwidth = width;
-	viewportheight = height;
+	viewportwidth1 = width1;
+	viewportheight1 = height1;
+	viewportwidth2 = width2;
+	viewportheight2 = height2;
     }
 
-    public SetWindow()
+    public window SetWindow( float width1, float height1, float width2, float height2)
     {
-
+	window w = new window;
+	w.windowheight1 = height1;
+	w.windowwidth1 = width1;
+	w.windowheight2 = height2;
+	w.windowwidth2 = width2;
     }
 
     public WindowToViewport(int xwin, int ywin)
